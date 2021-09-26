a9c3293846f558b767e0899e4c11de454774c83c
kalinkesilvio <silvio.kalinke60@gmail.com>
2021-09-26 12:26:28 +0200

    Room entity implemented

--- backend/src/main/java/at/htl/entity/Room.java
+++ backend/src/main/java/at/htl/entity/Room.java
@@ -1,4 +1,65 @@
 package at.htl.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String usage;
+    private boolean used;
+    private int surface;
+
+    public Room() {
+    }
+
+    public Room(Long id, String usage, boolean used, int surface) {
+        this.id = id;
+        this.usage = usage;
+        this.used = used;
+        this.surface = surface;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public int getSurface() {
+        return surface;
+    }
+
+    public void setSurface(int surface) {
+        this.surface = surface;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (used: %s), %d", this.usage, this.used, this.surface);
+    }
 }
