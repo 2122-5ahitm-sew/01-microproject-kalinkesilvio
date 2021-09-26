091951bd60873122e928f9725ae09b5207908d7b
kalinkesilvio <silvio.kalinke60@gmail.com>
2021-09-26 12:02:11 +0200

    FacilityManager entity implemented

--- backend/src/main/java/at/htl/entity/FacilityManager.java
+++ backend/src/main/java/at/htl/entity/FacilityManager.java
@@ -1,4 +1,65 @@
 package at.htl.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class FacilityManager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public FacilityManager() {
+    }
+
+    public FacilityManager(Long id, String firstName, String lastName, double salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s, %.2f", this.firstName, this.lastName, this.salary);
+    }
 }
