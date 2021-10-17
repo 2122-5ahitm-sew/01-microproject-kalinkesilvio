629eb77868253ddcc6d773a60f03bf71b5160bb5
kalinkesilvio <silvio.kalinke60@gmail.com>
2021-10-17 18:52:21 +0200

    FacilityManagerResource save and getAll Endpoints added

---
+++ backend/src/main/java/at/htl/boundary/FacilityManagerResource.java
@@ -0,0 +1,36 @@
+package at.htl.boundary;
+
+import at.htl.control.BuildingRepository;
+import at.htl.control.FacilityManagerRepository;
+import at.htl.entity.Building;
+import at.htl.entity.FacilityManager;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+
+@Path("manager")
+@Produces(MediaType.APPLICATION_JSON)
+public class FacilityManagerResource {
+    @Inject
+    FacilityManagerRepository facilityManagerRepository;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(FacilityManager facilityManager, @Context UriInfo uriInfo) {
+        facilityManagerRepository.create(facilityManager);
+        return Response.created(
+                URI.create(uriInfo.getPath() + "/" + facilityManager.getId())
+        ).build();
+    }
+
+    @GET
+    @Path("all")
+    public Response getAll() {
+        return Response.ok(facilityManagerRepository.getAll()).build();
+    }
+}
