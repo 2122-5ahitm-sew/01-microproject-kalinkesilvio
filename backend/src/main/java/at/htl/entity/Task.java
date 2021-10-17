710df73aa34cbe02e063e63cf73a804bd8fbdce9
kalinkesilvio <silvio.kalinke60@gmail.com>
2021-10-17 15:50:27 +0200

    Task Entity updated with correct Relations

--- backend/src/main/java/at/htl/entity/Task.java
+++ backend/src/main/java/at/htl/entity/Task.java
@@ -1,9 +1,9 @@
 package at.htl.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -16,12 +16,22 @@ public class Task {
     private boolean underProcess;
     private LocalDate dateOfCompletion;
 
+    @ManyToOne
+    @Cascade(CascadeType.ALL)
+    private FacilityManager facilityManager;
+
+    @ManyToOne
+    @Cascade(CascadeType.ALL)
+    private Issue issue;
+
     public Task() {
     }
 
-    public Task(boolean underProcess, LocalDate dateOfCompletion) {
+    public Task(boolean underProcess, LocalDate dateOfCompletion, FacilityManager facilityManager, Issue issue) {
         this.underProcess = underProcess;
         this.dateOfCompletion = dateOfCompletion;
+        this.facilityManager = facilityManager;
+        this.issue = issue;
     }
 
     public Long getId() {
@@ -38,6 +48,22 @@ public class Task {
 
     public LocalDate getDateOfCompletion() {
         return dateOfCompletion;
+    }
+
+    public FacilityManager getFacilityManager() {
+        return facilityManager;
+    }
+
+    public void setFacilityManager(FacilityManager facilityManager) {
+        this.facilityManager = facilityManager;
+    }
+
+    public Issue getIssue() {
+        return issue;
+    }
+
+    public void setIssue(Issue issue) {
+        this.issue = issue;
     }
 
     public void setDateOfCompletion(LocalDate dateOfCompletion) {
