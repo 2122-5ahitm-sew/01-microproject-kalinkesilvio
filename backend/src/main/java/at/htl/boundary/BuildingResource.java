48a960faf23b79c904bb80456b8b26bb477796a6
kalinkesilvio <silvio.kalinke60@gmail.com>
2021-10-17 19:02:48 +0200

    BuildingResource Endpoint delete implemented

--- backend/src/main/java/at/htl/boundary/BuildingResource.java
+++ backend/src/main/java/at/htl/boundary/BuildingResource.java
@@ -4,6 +4,7 @@ import at.htl.control.BuildingRepository;
 import at.htl.entity.Building;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -41,6 +42,23 @@ public class BuildingResource {
     @Path("all")
     public Response getAll() {
         return Response.ok(buildingRepository.getAll()).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(Building building, @Context UriInfo uriInfo) {
+        buildingRepository.save(building);
+        return Response
+                .created(URI.create(uriInfo.getPath() + "/" + building.getId()))
+                .build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response delete(@PathParam("id") long id) {
+        buildingRepository.remove(buildingRepository.getBuildingById(id));
+        return Response.ok().build();
     }
 
 }
